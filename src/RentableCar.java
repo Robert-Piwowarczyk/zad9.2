e4d51b9938454098dff5c849c1f1d6521b13af27
HelloWorld <robpiw@op.pl>
2020-05-08 10:48:32 +0200

    Pojazdy i wypożyczalnia

---
+++ src/RentableCar.java
@@ -0,0 +1,11 @@
+public class RentableCar {
+    @Override
+   public String rent(String firstName,String lastName,String id){
+    }
+    @Override
+    public boolean handOver(){
+    }
+    @Override
+    public boolean isRent(){
+    }
+}
---
+++ src/TestRentableCar.java
@@ -0,0 +1,7 @@
+public class TestRentableCar {
+    public static void main(String[] args) {
+        Vehicle[] vehicle = new Car[2];
+        vehicle[0] = new Car("Hyundai i10",2020,4);
+        vehicle[1] = new Car("Kia Ceed",2019,5);
+    }
+}
